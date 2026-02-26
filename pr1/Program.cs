04f47b20f57ca63001059fad4d5a0cb22b9fbe72
Maygdan <alesharet4253@gmail.com>
2026-02-26 22:42:09 +0300

    some Activity

---
+++ pr1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Data.SqlTypes;
+
+namespace Practice3
+{
+    public class A
+    {
+        protected string _name;
+        protected int _value;
+
+        public string Name=> _name;
+        private int Value=> _value;
+        public A(string name, int value)
+        {
+            _name=name;
+            _value=value;
+        }
+        public void Method1()
+        {
+            Console.WriteLine("Method class A");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine($"Name: {_name} \nValue: {_value}");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Also a method class A");
+        }
+    
+    }
+    public class B : A
+    {
+        private string _word;
+        private bool _isValid;
+        public string World=> _word;
+        
+        public B(string name, int value,string word):base(name,value)
+        {
+            _word=word;
+            _isValid=true;
+        }
+        public B(string name, int value,string word,bool isValid):this(name,value,word)
+        {
+            _word=word;
+            _isValid=isValid;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Hello");
+        }
+        
+    }
+    
+}
---
+++ pr1/pra2.cs
@@ -0,0 +1,13 @@
+using System;
+using Practice3;
+
+class Program
+{
+    static void Main()
+    {
+        A Man = new A("Alex",52);
+        Man.Print();
+        B Women= new B("April",23,"black");
+        Women.Print();
+    }
+}
