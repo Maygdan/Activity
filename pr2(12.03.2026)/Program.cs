995b51712e841bd5cce85fc68a1b75da0fa3e969
Maygdan <alesharet4253@gmail.com>
2026-03-12 17:25:34 +0300

    New practic

---
+++ pr2(12.03.2026)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace pr2;
+public abstract class Animal
+{
+   private int _id;
+   public abstract string Type{get;}
+
+   public Animal(int id)
+    {
+        _id=id;
+    }    
+
+    public abstract void Voice();
+
+    public virtual void Print()
+    {
+        Console.WriteLine("Animal ID"+_id);
+    }
+}
+public class Cat : Animal
+{
+    public Cat(int id):base(id){}
+
+    public override string Type => "Кошка";
+
+    public override void Voice()
+    {
+        Console.WriteLine("Meow");
+    }
+
+}
