a5b6afe972459c866dbbbd2de8ddf5e474830220
Maygdan <alesharet4253@gmail.com>
2026-03-13 08:43:56 +0300

    changes?

--- pr2(12.03.2026)/Program.cs
+++ pr2(12.03.2026)/Program.cs
@@ -1,33 +1,57 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices.Marshalling;
-
-namespace pr2;
+﻿namespace pr2;
 public abstract class Animal
 {
-   private int _id;
-   public abstract string Type{get;}
+    protected int _id;
+    private int _Count=0;
 
-   public Animal(int id)
-    {
-        _id=id;
-    }    
 
+    public abstract string Type { get; }
+    public abstract double Height {get;}
+    public abstract double Weight {get;}
+    public abstract string Name {get;}
+    public Animal()
+    { 
+        _id =_Count;
+        _Count++;
+    }
     public abstract void Voice();
+    public virtual void About()
+    {
+        Console.WriteLine($"ID: {_id}"); 
+    }
 
     public virtual void Print()
     {
-        Console.WriteLine("Animal ID"+_id);
+        Console.WriteLine("It's a cat, not a dog");
     }
 }
+
 public class Cat : Animal
 {
-    public Cat(int id):base(id){}
+    protected double _height;
+    protected double _weight;
+
+    protected string _name;
+
+    public Cat(double height, double weight,string name)
+    {
+        _name=name;
+        _height = height;
+        _weight = weight;
+    }
 
     public override string Type => "Кошка";
+    public override double Height => _height;
+    public override double Weight => _weight;
+    public override string Name => _name;
+
+    public override void About()
+    {
+        base.About();Console.WriteLine($"Name: {_name} \nWeight: {_weight} \nHeight: {_height} ");
+    }
 
     public override void Voice()
     {
         Console.WriteLine("Meow");
     }
-
 }
---
+++ pr2(12.03.2026)/Test.cs
@@ -0,0 +1,12 @@
+using pr2;
+using System;
+class Program
+{
+    static void Main()
+    {
+        Animal A=new Cat(24.7,35.9,"Alex");
+        A.About();
+        A.Print();
+        
+    }
+}
